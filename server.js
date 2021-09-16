a7a5c275545405a837bdd58fcb4a2578c9918831
Julia Evans <julia@jvns.ca>
2021-09-16 17:31:15 -0400

    test pg

--- server.js
+++ server.js
@@ -31,7 +31,6 @@ async function list(ctx) {
 }
 
 async function test_pg() {
-    console.log(process.env.DATABASE_URL)
     const client = new Client({
         connectionString: process.env.DATABASE_URL,
         ssl: {
@@ -39,14 +38,9 @@ async function test_pg() {
         }
     });
     await client.connect()
-
-    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-        if (err) throw err;
-        for (let row of res.rows) {
-            console.log(JSON.stringify(row));
-        }
-        client.end();
-    });
+    const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+    console.log(res.rows[0].message) // Hello world!
+    await client.end()
 }
 
 // listen
