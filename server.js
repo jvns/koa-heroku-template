a5850d042a8e5a5a5c517620eadd17cd47bff5a5
Julia Evans <julia@jvns.ca>
2021-09-16 17:25:24 -0400

    test pg

--- server.js
+++ server.js
@@ -25,7 +25,17 @@ app.use(router.routes());
 // index
 
 async function list(ctx) {
+  await test_pg();
   await ctx.render('index', {});
+}
+
+async function test_pg() {
+    const { Client } = require('pg')
+    const client = new Client()
+    await client.connect()
+    const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+    console.log(res.rows[0].message) // Hello world!
+    await client.end()
 }
 
 // listen
