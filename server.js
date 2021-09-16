12a1db3044848f28d33c716bb137927e4324073a
Julia Evans <julia@jvns.ca>
2021-09-16 17:18:53 -0400

    wip

--- server.js
+++ server.js
@@ -1,52 +1,37 @@
-var fs = require('fs');
-var logger = require('koa-logger');
-var serve = require('koa-static');
+const logger = require('koa-logger');
 const router = require('@koa/router')();
+const views = require('koa-views');
+const path = require('path');
+
+// setup views mapping .html
+// to the swig template engine
+
+const render = views(path.join(__dirname, './views'), {
+  map: { html: 'swig' }
+});
+
 const Koa = require('koa');
 const app = module.exports = new Koa();
 
-// Init logger
-app.use(logger());
+// middleware
 
-// Init router
-router.get('/', list)
-  .get('/get-test', get_test)
+app.use(logger());
+app.use(render);
+
+// route definitions
+
+router.get('/', list);
 
 app.use(router.routes());
 
-// Init public dir for css, js and etc..
-app.use(serve(__dirname + '/public'));
-
 /**
- * Index page
+ * Index
  */
 
 async function list(ctx) {
-  var indexHTML = fs.readFileSync(__dirname + '/public/index.html', 'utf-8');
-  //await test_pg();
-  console.log(ctx)
-  await ctx.render('list', indexHTML);
+  await ctx.render('index', {});
 }
 
-async function get_test(ctx) {
-  var data = {
-    'title': 'Koa test application',
-    'body': 'Hello World!'
-  };
+// listen
 
-  await ctx.render('show', data);
-}
-
-
-async function test_pg() {
-    const { Client } = require('pg')
-    const client = new Client()
-    await client.connect()
-    const res = await client.query('SELECT $1::text as message', ['Hello world!'])
-    console.log(res.rows[0].message) // Hello world!
-    await client.end()
-}
-
-var port = process.env.PORT || 3000;
-app.listen(port);
-console.log('Listening to %s', port);
+if (!module.parent) app.listen(3000);
